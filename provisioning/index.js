c54e6481810ab1cde88b8424c40101c86ac40186
Volatiana Rasoalinirina <volatiana.rasoalinirina@devngo.fr>
2025-09-18 06:57:57 +0300

    Align Node.js Elasticsearch client with v8 server

--- provisioning/index.js
+++ provisioning/index.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const crypto = require('crypto')
 const app = express()
 const port = 3001;
+const { Client } = require('@elastic/elasticsearch')
+const esClient = new Client({ node : 'http://localhost:9200'});
 
 app.post('/provision-index', async (req, res) => {
-    res.status(200).json({
-        status: 'TEST SUCCESS'
-    });
+    try {
+        indexName = crypto.randomBytes(6).toString('hex')
+        await esClient.indices.create({ index:  indexName});
+        res.status(201).json({
+            status: 'SUCCESS',
+            index_name: indexName
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: 'ERROR',
+            message : 'Could not create index'
+        })
+    }
 })
 
 app.listen(port, () => {
