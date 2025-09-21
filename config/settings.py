71916e82afdae79c677fdc0710c8c163cb3ee919
Volatiana Rasoalinirina <volatiana.rasoalinirina@devngo.fr>
2025-09-21 05:49:51 +0300

    add production database config

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
+import dj_database_url
+
 from decouple import config
 from pathlib import Path
 
@@ -87,6 +90,11 @@ DATABASES = {
     }
 }
 
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True,
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
